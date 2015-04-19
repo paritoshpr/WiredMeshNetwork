ee9aadbed480cfb776825f4c29418b7223567620
Paritosh Ramanan <paritosh.ramanan@gmail.com>
2015-04-19 03:53:17 -0400

    modified simulation files

--- src/WiredMeshNetworkSim/Man44WMS.java
+++ src/WiredMeshNetworkSim/Man44WMS.java
@@ -1,12 +1,5 @@
 package WiredMeshNetworkSim;
-/*      Copyright 2002 Arizona Board of regents on behalf of
- *                  The University of Arizona
- *                     All Rights Reserved
- *         (USE & RESTRICTION - Please read COPYRIGHT file)
- *
- *  Version    : DEVSJAVA 2.7
- *  Date       : 08-15-02
- */
+
 
 
 
--- src/WiredMeshNetworkSim/WiredNode.java
+++ src/WiredMeshNetworkSim/WiredNode.java
@@ -1,14 +1,4 @@
 package WiredMeshNetworkSim;
-/*      Copyright 2002 Arizona Board of regents on behalf of
- *                  The University of Arizona
- *                     All Rights Reserved
- *         (USE & RESTRICTION - Please read COPYRIGHT file)
- *
- *  Version    : DEVSJAVA 2.7
- *  Date       : 08-15-02
- */
-
-
 
 
 import simView.*;
